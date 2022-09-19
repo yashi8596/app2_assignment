fbfee5b51dc5b5187904c6d7d039e0f86f60ac07
yashi8596 <deletio.422shinta@gmail.com>
2022-09-19 13:23:51 +0000

    [Finish]課題STEP2まで画像以外終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
-  def show
+
+  def index
+    @users = User.all
+    @user = current_user
+    @new_book = Book.new
+
   end
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def edit
+
+  end
+
 end
